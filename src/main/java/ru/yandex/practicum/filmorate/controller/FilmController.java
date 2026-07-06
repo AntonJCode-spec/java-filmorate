39be9fab3fa822c51d9a881e450d7e6614448461
AntonKellerman <kellermankupriyanov@yandex.ru>
2026-07-06 15:01:11 +0300

    Commit 2

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -10,6 +11,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -23,64 +25,71 @@ public class FilmController {
 
     @PostMapping
     public Film addFilm(@RequestBody Film film) {
+        if (film.getName() == null) {
+            log.warn("Название фильма должно быть передано");
+            throw new ValidationException("Название фильма должно быть указано");
+        }
         validateFilm(film);
+        log.info("Валидация прошла успешно");
         film.setId(getNextId());
+        log.debug("Установлен ID {}", film.getId() );
         films.put(film.getId(), film);
+        log.info("Фильм с ID \"{}\" добавлен в базу", film.getId());
         return film;
     }
 
     @PutMapping
     public Film uppdateFilm(@RequestBody Film film) {
         if (film.getId() == null) {
+            log.warn("Ошибка валидации ID. ID не был передан в запросе");
             throw new ValidationException("ID должен быть указан");
         }
         if (!films.containsKey(film.getId())) {
+            log.warn("Полученный в запросе ID не содержится в базе. Получен ID \"{}\"", film.getId());
             throw new NotFoundException("Фильма с указанным ID не найдено");
         }
 
         Film newFilm = films.get(film.getId());
         updateFields(newFilm, film);
+        log.info("Значения полуй фильма с ID \"{}\" успешно обновлены", newFilm.getId());
         return newFilm;
     }
 
     private void validateFilm(Film film) throws ValidationException {
-        if (film.getName() == null || film.getName().isBlank()) {
+        if (film.getName() != null && film.getName().isBlank()) {
+            log.warn("Ошибка валидации названия. Было передано \"{}\"", film.getName());
             throw new ValidationException("название не может быть пустым");
         }
         if (film.getDescription() != null && film.getDescription().length() > 200) {
+            log.warn("Ошибка валидации описания. Переданная длина описания \"{}\"", film.getDescription().length());
             throw new ValidationException("максимальная длина описания — 200 символов");
         }
         if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(FIRST_FILM_DATE)) {
+            log.warn("ошибка валидации даты релиза фильма. Была получена дата релиза \"{}\"", film.getReleaseDate());
             throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года");
         }
         if (film.getDuration() != null && film.getDuration() <= 0) {
+            log.warn("Ошибка валидации продолжительности фильма. Полученное значение \"{}\"", film.getDuration());
             throw new ValidationException("продолжительность фильма должна быть положительным числом");
         }
     }
 
     private void updateFields(Film newFilm, Film film) {
+        validateFilm(film);
         if (film.getName() != null) {
-            if (film.getName().isBlank()) {
-                throw new ValidationException("название не может быть пустым");
-            }
+            log.debug("Для фильма с ID {} установлено новое название \"{}\"", film.getId(), film.getName());
             newFilm.setName(film.getName());
         }
         if (film.getDescription() != null) {
-            if (film.getDescription().length() > 200) {
-                throw new ValidationException("максимальная длина описания — 200 символов");
-            }
+            log.debug("Для фильма с ID {} установлено новое описание \"{}\"", film.getId(), film.getDescription());
             newFilm.setDescription(film.getDescription());
         }
         if (film.getReleaseDate() != null) {
-            if (film.getReleaseDate().isBefore(FIRST_FILM_DATE)) {
-                throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года");
-            }
+            log.debug("Для фильма с ID {} установлена новая дата релиза \"{}\"", film.getId(), film.getReleaseDate());
             newFilm.setReleaseDate(film.getReleaseDate());
         }
         if (film.getDuration() != null) {
-            if (film.getDuration() <= 0) {
-                throw new ValidationException("продолжительность фильма должна быть положительным числом");
-            }
+            log.debug("Для фильма с ID {} установлена новая продолжительность \"{}\"", film.getId(), film.getDuration());
             newFilm.setDuration(film.getDuration());
         }
     }
@@ -92,5 +101,4 @@ public class FilmController {
                 .orElse(0);
         return ++currentMax;
     }
-
 }
